d1469dbb3d8ad7d41d69da1d42a18014cb38bf10
Aliyu Haruna Khalifa <aliyoshk@hotmail.com>
2021-11-11 06:59:53 +0100

    Add project files.

---
+++ ExternalFileReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ExternalFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Directory.CreateDirectory("C:\\Assignment");
+                var path = "C:\\Assignment\\newrecords.txt";
+                var oldpath = "C:\\Assignment\\records.txt";
+
+                File.Create(path);
+                File.WriteAllText(path, ""); //Since the append is in loop these will clear all text on the file when ever run
+
+                foreach (var line in File.ReadAllLines(oldpath))
+                {
+                    var dMark = line.Split("-")[1];
+
+                    var markUp = new MarkUpgrader();
+                    var newMark = markUp.Upgrade(int.Parse(dMark));
+                    var newlineContent = line.Split("-")[1] + " - " + newMark;
+
+                    File.AppendAllLines(path, new string[] { line }); // Adding data to new created textfile
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
