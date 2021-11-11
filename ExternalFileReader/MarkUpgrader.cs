9f4c526fca76d1e7696407b9ebbdbd3c97b0a886
Aliyu Haruna Khalifa <41421796+aliyoshk@users.noreply.github.com>
2021-11-11 07:04:06 +0100

    Add files via upload

---
+++ ExternalFileReader/MarkUpgrader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExternalFileReader
+{
+    public class MarkUpgrader
+    {
+        public int Upgrade(int oldMark)
+        {
+            int newMark = 0;
+
+            if(oldMark >50)
+            {
+                newMark = oldMark + 5;
+            }
+            else
+            {
+                newMark = oldMark + 10;
+            }
+
+            ////int newMark = oldMark + 5;
+            ////if (oldMark < 50)
+            ////    newMark = oldMark + 10;
+
+            return oldMark;
+        }
+    }
+}
